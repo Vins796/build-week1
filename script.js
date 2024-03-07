e47d55230f5a2fcfb0d444dd6615b20694b61a1d
ChristianBello11 <christianbello789@gmail.com>
2024-03-07 17:55:51 +0100

    timer nelle domande

--- script.js
+++ script.js
@@ -240,8 +240,46 @@ function makeQuestion() {
   }
 }
   
-// Faccio la domanda al primo atterraggio
+
 makeQuestion();
+
+//timer
+function nextQuestion() {
+  contatore++; 
+  makeQuestion(); 
+  countdownTime = originaleCountdownTime; 
+  clearInterval(countdownInterval);
+  countdownInterval = setInterval(countdownFunction, 1000);
+  var cerchio1Element = document.querySelector(".cerchio1");
+  cerchio1Element.style.borderColor = "green";
+}
+
+
+var originaleCountdownTime = 60 * 1000;
+var countdownTime = originaleCountdownTime;
+
+function countdownFunction() {
+  var seconds = Math.floor(countdownTime / 1000);
+  var countdownElement = document.getElementById("countdown");
+  var cerchio1Element = document.querySelector(".cerchio1");
+
+  if (seconds <= 35 && seconds > 10) {
+      cerchio1Element.style.borderColor = "yellow";
+  } else if (seconds <= 10) {
+      cerchio1Element.style.borderColor = "red"; 
+  }
+
+  var secondsElement = document.querySelector(".seconds");
+  secondsElement.textContent = "00:" + seconds.toString().padStart(2, '0');
+  countdownTime -= 1000;
+
+  if (seconds === 0) {
+      clearInterval(countdownInterval);
+      nextQuestion();
+  }
+}
+
+var countdownInterval = setInterval(countdownFunction, 1000);
 
 
 
